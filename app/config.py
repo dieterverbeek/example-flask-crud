3e2f56496f11e0b041ed04aca26d63920a5d7a49
Dv <r0938875@student.thomasmore.be>
2024-10-23 22:43:02 +0200

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,8 +1,12 @@
 import os
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    SECRET_KEY = 'do-or-do-not-there-is-no-try'
-#    SECRET_KEY = os.environ.get('SECRET_KEY') or 'do-or-do-not-there-is-no-try'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('database-1.cb2sm4wk4177.us-east-1.rds.amazonaws.com') or 'mysql://admin:<adminproject>@database-1.cb2sm4wk4177.us-east-1.rds.amazonaws.com/<project>'
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'do-or-do-not-there-is-no-try')  # Gebruik een omgevingsvariabele voor de SECRET_KEY
+
+    # Zorg ervoor dat je de juiste database-parameters hier invult.
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'mysql://admin:<adminproject>@database-1.cb2sm4wk4177.us-east-1.rds.amazonaws.com:3306/project'
+    
     SQLALCHEMY_TRACK_MODIFICATIONS = False
