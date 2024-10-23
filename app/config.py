fb5c6ce4784bc4495dd2ac51d7a7cf8037bb3dce
Dv <r0938875@student.thomasmore.be>
2024-10-23 22:21:01 +0200

    Update config.py

--- app/config.py
+++ app/config.py
@@ -4,5 +4,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SECRET_KEY = 'do-or-do-not-there-is-no-try'
 #    SECRET_KEY = os.environ.get('SECRET_KEY') or 'do-or-do-not-there-is-no-try'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('database-1.cb2sm4wk4177.us-east-1.rds.amazonaws.com') or 'mysql://admin:<adminproject>@database-1.cb2sm4wk4177.us-east-1.rds.amazonaws.com/<project>'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
