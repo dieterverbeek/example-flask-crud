1e72ff433a4703c4b453cb9fd226cd07a2cffebe
Dv <r0938875@student.thomasmore.be>
2024-10-25 13:08:05 +0200

    Update config.py

--- app/config.py
+++ app/config.py
@@ -6,6 +6,11 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY', 'do-or-do-not-there-is-no-try')  # Gebruik een omgevingsvariabele voor de SECRET_KEY
 
     # Zorg ervoor dat je de juiste database-parameters hier invult.
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
         
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+
+#    SECRET_KEY = 'do-or-do-not-there-is-no-try'
+#    SECRET_KEY = os.environ.get('SECRET_KEY') or 'do-or-do-not-there-is-no-try'
