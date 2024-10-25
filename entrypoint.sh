c1bf5eb4d72980d0e5a6e178aa7d019d0d28f694
Dv <r0938875@student.thomasmore.be>
2024-10-25 11:13:01 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,3 +10,5 @@ flask db init || true  # init kan falen als de migraties al zijn gedaan
 flask db migrate -m "entries table"  # Dit is je specifieke migratie voor 'entries table'
 flask db upgrade  # Zorgt ervoor dat de database wordt bijgewerkt naar de laatste migraties
 
+# Start Gunicorn om de Flask-applicatie te serveren
+exec gunicorn -w 4 -b 0.0.0.0:8000 crudapp:app
