3844edef1fa7b2848a834df9f58c7d375d622c9d
Dv <r0938875@student.thomasmore.be>
2024-10-25 12:49:11 +0200

    Update config.py

--- app/config.py
+++ app/config.py
@@ -7,6 +7,5 @@ class Config(object):
 
     # Zorg ervoor dat je de juiste database-parameters hier invult.
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'mysql://admin:<adminproject>@database-1.cb2sm4wk4177.us-east-1.rds.amazonaws.com:3306/project'
-    
+        
     SQLALCHEMY_TRACK_MODIFICATIONS = False
