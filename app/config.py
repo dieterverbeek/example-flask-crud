473645feb6df794ffab7f8a67e1791f17f074a70
Dv <r0938875@student.thomasmore.be>
2024-10-27 20:43:17 +0100

    Update config.py

--- app/config.py
+++ app/config.py
@@ -7,10 +7,4 @@ class Config(object):
 
     # Zorg ervoor dat je de juiste database-parameters hier invult.
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
-
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-
-
-#    SECRET_KEY = 'do-or-do-not-there-is-no-try'
-#    SECRET_KEY = os.environ.get('SECRET_KEY') or 'do-or-do-not-there-is-no-try'
